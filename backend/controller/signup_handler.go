1fb2e27bae34ceb8e77a4510b8061c435ac47db1
John <johneliud4@gmail.com>
2025-02-02 20:12:03 +0300

    fix: SignupHandler execution based on the http request method.

--- backend/controller/signup_handler.go
+++ backend/controller/signup_handler.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/johneliud/forum/backend/util"
 	"github.com/johneliud/forum/database"
@@ -18,54 +19,67 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := r.ParseForm(); err != nil {
-		log.Printf("Failed parsing form: %v\n", err)
-		ErrorHandler(w, "Bad Request", http.StatusBadRequest)
+	switch r.Method {
+	case http.MethodGet:
+		tmpl, err := template.ParseFiles("frontend/templates/sign-up.html")
+		if err != nil {
+			log.Printf("Failed parsing template: %v\n", err)
+			ErrorHandler(w, "Page Not Found", http.StatusNotFound)
+			return
+		}
+
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			log.Printf("Failed to execute template: %v\n", err)
+			ErrorHandler(w, "Something Unexpected Happened. Try Again Later", http.StatusInternalServerError)
+			return
+		}
+	case http.MethodPost:
+		if err := r.ParseForm(); err != nil {
+			log.Printf("Failed parsing form: %v\n", err)
+			ErrorHandler(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+
+		name, email, password, confirmedPassword := r.FormValue("name"), r.FormValue("email"), r.FormValue("password"), r.FormValue("confirmed-password")
+
+		if err := util.ValidateInput(name, email, password, confirmedPassword); err != nil {
+			log.Printf("Failed validating form values: %v\n", err)
+			ErrorHandler(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+
+		hashedPassword, err := util.HashPassword(password)
+		if err != nil {
+			log.Printf("Failed hashing passord: %v\n", err)
+			ErrorHandler(w, "Something Unexpected Happened. Try Again Later", http.StatusInternalServerError)
+			return
+		}
+
+		if database.DB == nil {
+			log.Println("Database not initialized")
+			return
+		}
+
+		stmt, err := database.DB.Prepare("INSERT INTO users(name, email, password) VALUES (?, ?, ?)")
+		if err != nil {
+			log.Printf("Failed preparing statement to database: %v\n", err)
+			ErrorHandler(w, "Something Unexpected Happened. Try Again Later", http.StatusInternalServerError)
+			return
+		}
+		defer stmt.Close()
+
+		_, err = stmt.Exec(name, email, hashedPassword)
+		if err != nil {
+			log.Printf("Failed executing statement: %v\n", err)
+			ErrorHandler(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+
+		http.Redirect(w, r, "/sign-in", http.StatusSeeOther)
+	default:
+		log.Println("Invalid request method")
+		ErrorHandler(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
-
-	name, email, password, confirmedPassword := r.FormValue("name"), r.FormValue("email"), r.FormValue("password"), r.FormValue("confirmed-password")
-
-	log.Printf(`Received Form Data:
-Name: %s
-Email: %s
-Password: %s
-Confirmed Password: %s
-`,
-		name, email, password, confirmedPassword)
-
-	if err := util.ValidateInput(name, email, password, confirmedPassword); err != nil {
-		log.Printf("Failed validating form values: %v\n", err)
-		ErrorHandler(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-
-	hashedPassword, err := util.HashPassword(password)
-	if err != nil {
-		log.Printf("Failed hashing passord: %v\n", err)
-		ErrorHandler(w, "Something Unexpected Happened. Try Again Later", http.StatusInternalServerError)
-		return
-	}
-
-	if database.DB == nil {
-		log.Println("Database not initialized")
-		return
-	}
-
-	stmt, err := database.DB.Prepare("INSERT INTO users(name, email, password) VALUES (?, ?, ?)")
-	if err != nil {
-		log.Printf("Failed preparing statement to database: %v\n", err)
-		ErrorHandler(w, "Something Unexpected Happened. Try Again Later", http.StatusInternalServerError)
-		return
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(name, email, hashedPassword)
-	if err != nil {
-		log.Printf("Failed executing statement: %v\n", err)
-		ErrorHandler(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-
-	http.Redirect(w, r, "/sign-in", http.StatusSeeOther)
 }
