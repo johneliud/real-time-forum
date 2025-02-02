eb7b74a3a9aa780b4a7083e422c1c800cbb354cb
John <johneliud4@gmail.com>
2025-02-02 19:23:06 +0300

    refactor: move signup code logic to its own separate file.

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -30,13 +30,3 @@ const savedTheme = localStorage.getItem('theme') || 'light';
 applyTheme(savedTheme);
 
 document.querySelector('.theme-toggler').addEventListener('click', toggleTheme);
-
-document.getElementById('signup-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-  document.getElementById('success-message').style.display = 'block';
-  this.reset(); // Clear form fields
-
-  setTimeout(() => {
-    document.getElementById('success-message').style.display = 'none';
-  }, 3000);
-});
