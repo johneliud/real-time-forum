7fb1de73ad3df14e094f68a986d49ec77a6281ad
johneliud <johneliud4@gmail.com>
2025-03-04 22:10:12 +0300

    refactor: modify script to only execute once every DOM element has fully loaded.

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,39 +1,43 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-hamburgerMenu.addEventListener('click', () => {
-  console.log('Clicked');
+document.addEventListener("DOMContentLoaded", () => {
+  function applyTheme(theme) {
+    if (theme === "dark") {
+      document.body.classList.add("dark-theme");
+    } else {
+      document.body.classList.remove("dark-theme");
+    }
+  }
 
-  hamburgerMenu.classList.toggle('active');
+  function toggleTheme() {
+    let currentTheme = "";
+    let newTheme = "";
+
+    if (document.body.classList.contains("dark-theme")) {
+      currentTheme = "dark";
+    } else {
+      currentTheme = "light";
+    }
+
+    if (currentTheme === "dark") {
+      newTheme = "light";
+    } else {
+      newTheme = "dark";
+    }
+
+    applyTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  }
+  const savedTheme = localStorage.getItem("theme") || "light";
+  applyTheme(savedTheme);
+
+  document
+    .querySelector(".theme-toggler")
+    .addEventListener("click", toggleTheme);
+
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+  if (hamburgerMenu) {
+    hamburgerMenu.addEventListener("click", () => {
+      document.querySelector(".hamburger-menu").classList.toggle("active");
+    });
+  }
 });
-
-const applyTheme = (theme) => {
-  if (theme === 'dark') {
-    document.body.classList.add('dark-theme');
-  } else {
-    document.body.classList.remove('dark-theme');
-  }
-};
-
-// toggleTheme uses the value stored in the browsers local storage to determine the current theme.
-const toggleTheme = () => {
-  let currentTheme = '';
-  let newTheme = '';
-
-  if (document.body.classList.contains('dark-theme')) {
-    currentTheme = 'dark';
-  } else {
-    currentTheme = 'light';
-  }
-
-  if (currentTheme === 'dark') {
-    newTheme = 'light';
-  } else {
-    newTheme = 'dark';
-  }
-
-  applyTheme(newTheme);
-  localStorage.setItem('theme', newTheme);
-};
-const savedTheme = localStorage.getItem('theme') || 'light';
-applyTheme(savedTheme);
-
-document.querySelector('.theme-toggler').addEventListener('click', toggleTheme);
