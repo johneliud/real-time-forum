69706f3ce6aaf7f91060c1397eb0b104bc3eb056
johneliud <johneliud4@gmail.com>
2025-03-08 17:41:54 +0300

    add: User struct containing related user fields.

--- backend/model/model.go
+++ backend/model/model.go
@@ -1,5 +1,16 @@
 package model
 
+type User struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	NickName  string `json:"nickName"`
+	Gender    string `json:"gender"`
+	Age       int    `json:"age"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
 type SignupRequest struct {
 	FirstName         string `json:"firstName"`
 	LastName          string `json:"lastName"`
