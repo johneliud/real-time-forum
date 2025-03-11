3e055b673db69023c4151fd79a39a66718e0d58e
johneliud <johneliud4@gmail.com>
2025-03-11 10:58:48 +0300

    refactor: modify code to use a dynamic value when accessing the program's server instead of a hard coded value.

--- main.go
+++ main.go
@@ -12,21 +12,28 @@ import (
 )
 
 func main() {
-	err := util.LoadEnv(".env")
-	if err != nil {
-		fmt.Println("Error loading .env file:", err)
-		return
-	}
+	var err error
 
 	err = logger.NewLogger("data/app.log", slog.LevelDebug)
 	if err != nil {
 		panic(err)
 	}
 
-	database.InitDB()
+	err = util.LoadEnv(".env")
+	if err != nil {
+		logger.Error("Failed loading .env file: %v", err)
+		return
+	}
 
+	database.InitDB()
 	routes.Routes()
 
-	logger.Info("Server started at http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	port, err := util.ValidatePort()
+	if err != nil {
+		logger.Error("Error validating port: %v", err)
+	}
+
+	serverMessage := fmt.Sprintf("Server started at http://localhost%v", port)
+	logger.Info(serverMessage)
+	http.ListenAndServe(port, nil)
 }
