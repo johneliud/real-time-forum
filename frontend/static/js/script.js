3d300eb62f8831cc361e06a87a7e1ead2c14c034
johneliud <johneliud4@gmail.com>
2025-03-11 11:21:54 +0300

    add: toggleHamburgerMenu() function responsible for switching the hamburger menu state.

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,41 +1,56 @@
 export function initThemeToggler() {
-  const savedTheme = localStorage.getItem("theme") || "light";
+  const savedTheme = localStorage.getItem('theme') || 'light';
   applyTheme(savedTheme);
 
-  document.body.addEventListener("click", (e) => {
-    if (e.target.closest(".theme-toggler")) {
+  document.body.addEventListener('click', (e) => {
+    if (e.target.closest('.theme-toggler')) {
       toggleTheme();
     }
   });
 
   function applyTheme(theme) {
-    if (theme === "dark") {
-      document.body.classList.add("dark-theme");
+    if (theme === 'dark') {
+      document.body.classList.add('dark-theme');
     } else {
-      document.body.classList.remove("dark-theme");
+      document.body.classList.remove('dark-theme');
     }
   }
 
   function toggleTheme() {
-    const currentTheme = document.body.classList.contains("dark-theme")
-      ? "dark"
-      : "light";
-    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    const currentTheme = document.body.classList.contains('dark-theme')
+      ? 'dark'
+      : 'light';
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
 
     applyTheme(newTheme);
-    localStorage.setItem("theme", newTheme);
+    localStorage.setItem('theme', newTheme);
   }
 }
 
 export function showMessage(message, isSuccess) {
-  const messagePopup = document.getElementById("message-popup");
+  const messagePopup = document.getElementById('message-popup');
 
   messagePopup.textContent = message;
-  messagePopup.classList.remove("success", "error");
+  messagePopup.classList.remove('success', 'error');
 
-  messagePopup.classList.add("show", isSuccess ? "success" : "error");
+  messagePopup.classList.add('show', isSuccess ? 'success' : 'error');
 
   setTimeout(() => {
-    messagePopup.classList.remove("show", "success", "error");
+    messagePopup.classList.remove('show', 'success', 'error');
   }, 3000);
+}
+
+export function toggleHamburgerMenu() {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const menuContent = document.querySelector('.menu-content');
+
+  if (hamburgerMenu) {
+    hamburgerMenu.addEventListener('click', () => {
+      console.log("Clicked");
+      
+      hamburgerMenu.classList.toggle('active');
+      menuContent.style.display =
+        menuContent.style.display === 'block' ? 'none' : 'block';
+    });
+  }
 }
