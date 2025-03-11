2ebc4fe591b313477775ba7866c4023f91c1fcd1
johneliud <johneliud4@gmail.com>
2025-03-11 11:41:37 +0300

    refactor: modify code to invoke functions from other modules.

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -1,6 +1,7 @@
-import { initSignupValidation } from './signup_validation.js';
-import { initSigninValidation } from './signin_validation.js';
 import { initThemeToggler, toggleHamburgerMenu } from './script.js';
+import { homeView } from './home_view.js';
+import { signUpView } from './signup_view.js';
+import { signInView } from './signin_view.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const router = new Router();
@@ -96,169 +97,10 @@ class Router {
 }
 
 // Renders the home view of the application.
-async function homeView() {
-  const app = document.getElementById('app');
-
-  // Set the inner HTML of the app element to display the home view content
-  app.innerHTML = `
-      <div class="home-container">
-          <h1>Welcome to Real Time Forum</h1>
-      </div>
-  `;
-}
+homeView();
 
 // Function to render the sign up view.
-async function signUpView() {
-  const app = document.getElementById('app');
-
-  // Render sign up form
-  app.innerHTML = `
-      <p class="message-popup" id="message-popup"></p>
-      <div class="form-container">
-          <h2>Sign Up</h2>
-          <form id="signup-form" novalidate>
-              <div class="user-names">
-                  <div class="input-group">
-                      <label for="first-name">First Name</label>
-                      <input type="text" id="first-name" name="first-name" required />
-                  </div>
-
-                  <div class="input-group">
-                      <label for="last-name">Last Name</label>
-                      <input type="text" id="last-name" name="last-name" required />
-                  </div>
-              </div>
-
-              <div class="name-gender-age">
-                  <div class="input-group">
-                      <label for="nick-name">Nickname</label>
-                      <input type="text" id="nick-name" name="nick-name" required />
-                  </div>
-
-                  <div class="input-group">
-                      <label for="gender">Gender</label>
-                      <select id="gender" name="gender" required>
-                          <option value="">Select Gender</option>
-                          <option value="male">Male</option>
-                          <option value="female">Female</option>
-                          <option value="prefer not to say">Prefer not to say</option>
-                      </select>
-                  </div>
-
-                  <div class="input-group">
-                      <label for="age">Age</label>
-                      <input
-                          type="number"
-                          id="age"
-                          name="age"
-                          required
-                          min="13"
-                          max="120"
-                      />
-                  </div>
-              </div>
-
-              <div class="input-group">
-                  <label for="email">Email</label>
-                  <input type="email" id="email" name="email" required />
-              </div>
-
-              <div class="password">
-                  <div class="input-group">
-                      <label for="password">Password</label>
-                      <div class="password-wrapper">
-                          <input type="password" id="password" name="password" required />
-                          <button
-                              type="button"
-                              class="toggle-password-visibility"
-                              data-target="password"
-                          >
-                              <box-icon name="show"></box-icon>
-                          </button>
-                      </div>
-                  </div>
-
-                  <div class="input-group">
-                      <label for="confirmed-password">Confirm Password</label>
-                      <div class="password-wrapper">
-                          <input
-                              type="password"
-                              id="confirmed-password"
-                              name="confirmed-password"
-                              required
-                          />
-                          <button
-                              type="button"
-                              class="toggle-password-visibility"
-                              data-target="confirmed-password"
-                          >
-                              <box-icon name="show"></box-icon>
-                          </button>
-                      </div>
-                  </div>
-              </div>
-
-              <button type="submit" class="sign-up-btn btn">Create Account</button>
-          </form>
-
-          <p class="switch-form">
-              Already have an account? <a href="/sign-in" data-link>Sign In</a>
-          </p>
-      </div>
-  `;
-
-  // Initialize the sign up form validation
-  setTimeout(() => {
-    initSignupValidation();
-  }, 0);
-}
+signUpView();
 
 // Function to render the sign in view.
-async function signInView() {
-  const app = document.getElementById('app');
-
-  // Render sign in form
-  app.innerHTML = `
-      <p class="message-popup" id="message-popup"></p>
-      <div class="form-container">
-        <h2>Sign In</h2>
-        <form action="/sign-in" id="signin-form" method="POST">
-          <div class="input-group">
-            <label for="email-or-nickname">Email or Nickname</label>
-            <input
-              type="text"
-              id="email-or-nickname"
-              name="email-or-nickname"
-              required
-            />
-          </div>
-
-          <div class="input-group">
-            <label for="password">Password</label>
-            <div class="password-wrapper">
-              <input type="password" id="password" name="password" required />
-              <button
-                type="button"
-                class="toggle-password-visibility"
-                data-target="password"
-              >
-                <box-icon name="show"></box-icon>
-              </button>
-            </div>
-          </div>
-
-          <div class="line"></div>
-          <button type="submit" class="sign-in-btn btn">Sign In</button>
-        </form>
-
-        <p class="switch-form">
-          Don't have an account? <a href="/sign-up">Sign Up</a>
-        </p>
-      </div>
-  `;
-
-  // Initialize the sign in form validation
-  setTimeout(() => {
-    initSigninValidation();
-  }, 0);
-}
+signInView();
