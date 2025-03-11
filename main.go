ac43b7633b0dfb660f4661a12a38ab4acffb5493
johneliud <johneliud4@gmail.com>
2025-03-11 11:11:46 +0300

    refactor: create data folder before program executes.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/johneliud/real-time-forum/backend/logger"
 	"github.com/johneliud/real-time-forum/backend/routes"
@@ -14,24 +15,27 @@ import (
 func main() {
 	var err error
 
+	_ = os.Mkdir("data", 0o700)
+
 	err = logger.NewLogger("data/app.log", slog.LevelDebug)
 	if err != nil {
 		panic(err)
 	}
 
+	port, err := util.ValidatePort()
+	if err != nil {
+		logger.Error("Error validating port:", err)
+		return
+	}
+
 	err = util.LoadEnv(".env")
 	if err != nil {
-		logger.Error("Failed loading .env file: %v", err)
+		logger.Error("Failed loading .env file:", err)
 		return
 	}
 
 	database.InitDB()
 	routes.Routes()
-
-	port, err := util.ValidatePort()
-	if err != nil {
-		logger.Error("Error validating port: %v", err)
-	}
 
 	serverMessage := fmt.Sprintf("Server started at http://localhost%v", port)
 	logger.Info(serverMessage)
