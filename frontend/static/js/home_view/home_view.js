0800dd8ab08ed7d61e508c2287f1bc08b1d37b65
johneliud <johneliud4@gmail.com>
2025-03-28 04:47:59 +0300

    update: homeView() function to invoke postCreationView() function.

--- frontend/static/js/home_view/home_view.js
+++ frontend/static/js/home_view/home_view.js
@@ -1,19 +1,17 @@
-import { categoriesView } from './categories.js';
+import { categoriesView } from "./categories.js";
+import { postCreationView } from "./post_creation.js";
 
 // Renders the home view of the application.
 export async function homeView() {
-  const app = document.getElementById('app');
+  const app = document.getElementById("app");
 
-  const aside = document.createElement('aside');
-  aside.id = 'sidebar';
-  aside.className = 'sidebar';
+  const aside = document.createElement("aside");
+  aside.id = "sidebar";
+  aside.className = "sidebar";
 
   app.parentNode.insertBefore(aside, app);
 
   categoriesView();
 
-  // Set the inner HTML of the app element to display the home view content
-  app.innerHTML = `
-          <h1>Real Time Forum</h1>
-  `;
+  postCreationView();
 }
